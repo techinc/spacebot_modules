d2bd1dc69867b2c5d0fbc2f095eb21ffe112ab88
realitygaps <realitygaps@yahoo.com>
2013-08-21 23:00:52 +0200

    Adding back some logic

--- spacestatechange.py
+++ spacestatechange.py
@@ -2,6 +2,7 @@ import urllib2
 import time
 import willie
 
+statetracking = 0
 
 def checkstate():
   response = urllib2.urlopen('http://techinc.nl/space/spacestate')
@@ -17,17 +18,23 @@ def changestate(state):
 
 @willie.module.commands('trackstate')
 def trackstate(bot, trigger):
-  currentstate = checkstate()
+  global statetracking
+  if statetracking != 1:
+    statetracking = 1
+    currentstate = checkstate()
+    bot.say('Now tracking spacestate')
 
-  while True:
-    newstate = checkstate()
-    if newstate != currentstate:
-      bot.say('The space is now ' + newstate)
-      newtopic = 'Welcome to Technologia Incognita, we are ' + newstate + '. https://www.techinc.nl/ - Social night every Wednesday at ACTA'
-      channel = trigger.sender.lower()
-      bot.write(('TOPIC', channel + ' :' + newtopic))
-      currentstate = newstate
-    time.sleep(5)
+    while True:
+      newstate = checkstate()
+      if newstate != currentstate:
+        bot.say('The space is now ' + newstate)
+        newtopic = 'Welcome to Technologia Incognita, we are ' + newstate + '. https://www.techinc.nl/ - Social night every Wednesday at ACTA'
+        channel = trigger.sender.lower()
+        bot.write(('TOPIC', channel + ' :' + newtopic))
+        currentstate = newstate
+      time.sleep(5)
+  else:
+    bot.say('Already tracking spacestate')
 
 @willie.module.commands('togglestate')
 def togglestate(bot, trigger):
