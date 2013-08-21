b25173a7036be675cb011cf83d19875685d4eb3e
realitygaps <realitygaps@yahoo.com>
2013-08-21 22:37:39 +0200

    Adding togglestate

--- spacestatechange.py
+++ spacestatechange.py
@@ -2,30 +2,40 @@ import urllib2
 import time
 import willie
 
-statetracking = 0
 
 def checkstate():
   response = urllib2.urlopen('http://techinc.nl/space/spacestate')
   html = response.read()
   return html
 
+def changestate(state):
+  print 'State: %s' % state
+  response = urllib2.urlopen('http://techinc.nl/space/index.php?state=%s&key=securepassword' % state)
+  html = response.read()
+  return html
+
+
 @willie.module.commands('trackstate')
 def trackstate(bot, trigger):
-  global statetracking
-  if statetracking != 1:
-    statetracking = 1
-    currentstate = checkstate()
-    bot.say('Now tracking spacestate') 
- 
-    while True:
-      newstate = checkstate()
-      if newstate != currentstate:
-        bot.say('The space is now ' + newstate)
-        newtopic = 'Welcome to Technologia Incognita, we are ' + newstate + '. https://www.techinc.nl/ - Social night every Wednesday at ACTA'
-        channel = trigger.sender.lower()
-        bot.write(('TOPIC', channel + ' :' + newtopic))
-        currentstate = newstate
-      time.sleep(5)
+  currentstate = checkstate()
+
+  while True:
+    newstate = checkstate()
+    if newstate != currentstate:
+      bot.say('The space is now ' + newstate)
+      newtopic = 'Welcome to Technologia Incognita, we are ' + newstate + '. https://www.techinc.nl/ - Social night every Wednesday at ACTA'
+      channel = trigger.sender.lower()
+      bot.write(('TOPIC', channel + ' :' + newtopic))
+      currentstate = newstate
+    time.sleep(5)
+
+@willie.module.commands('togglestate')
+def togglestate(bot, trigger):
+  currentstate = checkstate()
+  bot.say('Changing Spacestate')
+  if currentstate == 'open':
+    state = 'closed'
   else:
-    bot.say('Already tracking spacestate')
+    state = 'open'
+  changestate(state)
 
