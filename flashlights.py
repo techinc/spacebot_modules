a32c15b0288482cad229a185bfacfbd7c622bf10
realitygaps <realitygaps@yahoo.com>
2013-12-22 20:57:51 +0100

    Adding spacealert flashlights.py

---
+++ flashlights.py
@@ -0,0 +1,14 @@
+import urllib2
+import time
+import willie
+from willie.module import commands, interval
+
+@willie.module.commands('alertspace')
+def spacestate(bot, trigger):
+  opener = urllib2.build_opener(urllib2.HTTPHandler)
+  request = urllib2.Request('http://10.0.20.32/api/0123456789abdcef0123456789abcdef/groups/0/action', data='{    "alert": "lselect" }')
+  request.add_header('Content-Type', 'text/json')
+  request.get_method = lambda: 'PUT'
+  url = opener.open(request)
+  bot.say('The space has been alerted!')
+
