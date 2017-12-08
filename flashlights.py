7add6e60fe3d19d6bfbd0b89c1b6826ec5af6422
chotee <chotee@openended.eu>
2017-12-09 00:10:51 +0100

    Adding ledslie http alert to be triggered with space alert.
    (cherry picked from commit 4b503b6)

--- flashlights.py
+++ flashlights.py
@@ -1,16 +1,32 @@
 import urllib2
-import time
+import json
+# import time
 import willie
-from willie.module import commands, interval
+
+# from willie.module import commands, interval
 
 @willie.module.commands('alertspace')
-@willie.module.example('.alertspace','alertspace')
+@willie.module.example('.alertspace open door', 'alertspace')
 def spacestate(bot, trigger):
-  """Alerts the space by flashing the lights a few times"""
-  opener = urllib2.build_opener(urllib2.HTTPHandler)
-  request = urllib2.Request('http://10.0.20.32/api/0123456789abdcef0123456789abcdef/groups/0/action', data='{    "alert": "lselect" }')
-  request.add_header('Content-Type', 'text/json')
-  request.get_method = lambda: 'PUT'
-  url = opener.open(request)
-  bot.say('The space has been alerted!')
+    """Alerts the space by flashing the lights a few times"""
+    _alert_lamps()
+    _alert_ledslie(trigger.nick, trigger.groups()[1:])
+    bot.say('The space has been alerted!')
 
+
+def _alert_lamps():
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+    request = urllib2.Request('http://10.0.20.32/api/0123456789abdcef0123456789abcdef/groups/0/action',
+                              data='{ "alert": "lselect" }')
+    request.add_header('Content-Type', 'text/json')
+    request.get_method = lambda: 'PUT'
+    opener.open(request)
+
+
+def _alert_ledslie(nick, message):
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+    data = {"who": nick, "text": message}
+    request = urllib2.Request('http://ledslie.ti/alert', data=json.dumps(data))
+    request.add_header('Content-Type', 'text/json')
+    request.get_method = lambda: 'POST'
+    opener.open(request)
